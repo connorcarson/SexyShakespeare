f350b362a28e6382b878321d872a8f775aeea9b0
ElizabethBallou <epb267@nyu.edu>
2019-05-01 16:07:31 -0400

    intro animation working

---
+++ Something Sexy/Assets/Scripts/IntroAnimationBoss.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using DG.Tweening.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroAnimationBoss : MonoBehaviour
+{
+    public GameObject leftCurtain;
+    public GameObject rightCurtain;
+    public GameObject centerCurtain;
+    public GameObject button;
+    private AudioSource audioSource;
+    private AudioClip thudSound;
+    public GameObject title;
+
+    private float seconds;
+    
+    // Start is called before the first frame update
+    void Start()
+    {   
+        audioSource = GetComponent<AudioSource>();
+        thudSound = (AudioClip)Resources.Load("thud sound");
+        audioSource.clip = thudSound;
+        leftCurtain.transform.DOMove(new Vector3(-7.85f, -1.25f, 0f), .5f);
+        StartCoroutine(DelayedSound(.3f));
+        rightCurtain.transform.DOMove(new Vector3(7.62f, -1.25f, 0f), .5f).SetDelay(1f);
+        StartCoroutine(DelayedSound(1.3f));
+        centerCurtain.transform.DOMove(new Vector3(0f, 3.7f, 0f), .5f).SetDelay(2f);
+        StartCoroutine(DelayedSound(2.2f));
+        StartCoroutine(DelayedTitle(2.5f));
+        StartCoroutine(DelayedButton(4f));
+    }
+
+    IEnumerator DelayedSound(float seconds)
+    {
+        yield return new WaitForSeconds (seconds);
+        audioSource.Play();
+             
+    }
+
+    IEnumerator DelayedTitle(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        title.SetActive(true);
+    }
+
+    IEnumerator DelayedButton(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        button.SetActive(true);
+    }
+
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
