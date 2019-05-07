8186bc230ad8dddd41e08e97072e3e47b1c91149
ElizabethBallou <epb267@nyu.edu>
2019-05-06 22:40:25 -0400

    Added tooltips for top row of character icons

---
+++ Something Sexy/Assets/Scripts/TooltipController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+using DG.Tweening;
+using DG.Tweening.Core;
+using TMPro;
+using Image = UnityEngine.UI.Image;
+
+public class TooltipController : MonoBehaviour
+{
+    public Image tooltip;
+    public TextMeshProUGUI tooltipText;
+    private Color parchmentColor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        parchmentColor = new Color(255f, 246f, 225f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    private void OnMouseOver()
+    {
+        tooltip.DOColor(parchmentColor, .5f);
+        tooltipText.DOColor(Color.black, .5f);
+    }
+
+    private void OnMouseExit()
+    {
+        tooltip.DOColor(Color.clear, .5f);
+        tooltipText.DOColor(Color.clear, .5f);
+    }
+}
