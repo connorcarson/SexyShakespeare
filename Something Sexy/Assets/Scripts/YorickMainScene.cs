8ca9cc5db0fabd3306b9a6a4578b021913174c38
ElizabethBallou <epb267@nyu.edu>
2019-05-13 11:37:45 -0400

    Put curtain over contestants. Started working on Yorick's transitional text for the main scene.

---
+++ Something Sexy/Assets/Scripts/YorickMainScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using DG.Tweening.Core;
+
+public class YorickMainScene : MonoBehaviour
+{
+    public Text transitionalWriting;
+    public Image textBox;
+    public Image textBoxTail;
+    public string[] transitionalText;
+
+    private int indexLocation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        indexLocation = 0;
+        transitionalWriting.text = transitionalText[0];
+
+        textBox.DOColor(Color.white, .5f);
+        textBoxTail.DOColor(Color.white, .5f);
+        transitionalWriting.DOColor(Color.black, .5f);
+    }
+
+    public void CallNewLine()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
