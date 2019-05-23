fb745f9811e5622d8779deb24eb30d608a418289
ElizabethBallou <epb267@nyu.edu>
2019-05-23 11:25:09 -0400

    All sprites in the intro scene are now UI images. DOMove changed to DOAnchorPos.

--- Something Sexy/Assets/Scripts/IntroAnimationBoss.cs
+++ Something Sexy/Assets/Scripts/IntroAnimationBoss.cs
@@ -9,22 +9,22 @@ using UnityEngine.UI;
 
 public class IntroAnimationBoss : MonoBehaviour
 {
-    public GameObject leftCurtain;
-    public GameObject rightCurtain;
-    public GameObject centerCurtain;
+    public Image leftCurtain;
+    public Image rightCurtain;
+    public Image centerCurtain;
     public GameObject button;
     public TextMeshProUGUI title;
 
-    public SpriteRenderer Antony;
-    public SpriteRenderer Cleopatra;
-    public SpriteRenderer Beatrice;
-    public SpriteRenderer Benedick;
-    public SpriteRenderer Hamlet;
-    public SpriteRenderer Ophelia;
-    public SpriteRenderer Juliet;
-    public SpriteRenderer Romeo;
-    public SpriteRenderer Petruchio;
-    public SpriteRenderer Kate;
+    public Image Antony;
+    public Image Cleopatra;
+    public Image Beatrice;
+    public Image Benedick;
+    public Image Hamlet;
+    public Image Ophelia;
+    public Image Juliet;
+    public Image Romeo;
+    public Image Petruchio;
+    public Image Kate;
     
     private float seconds;
     
@@ -33,18 +33,18 @@ public class IntroAnimationBoss : MonoBehaviour
     {
         title.color = Color.clear;
 
-        leftCurtain.transform.DOMove(new Vector3(-7.85f, -1.25f, 0f), .5f);
-        rightCurtain.transform.DOMove(new Vector3(7.62f, -1.25f, 0f), .5f).SetDelay(1f);
-        centerCurtain.transform.DOMove(new Vector3(0f, 4.25f, 0f), .5f).SetDelay(2f);
+        leftCurtain.rectTransform.DOAnchorPos(new Vector2(-383.46f, -45.368f), .5f );
+        rightCurtain.rectTransform.DOAnchorPos(new Vector2(384f, -45.368f), .5f).SetDelay(1f);
+        centerCurtain.rectTransform.DOAnchorPos(new Vector2(-6.150024f, 211f), .5f).SetDelay(2f);
         title.DOColor(Color.white, 1.5f).SetDelay(2.8f);
         Antony.DOColor(Color.white, .5f).SetDelay(2.8f);
         Cleopatra.DOColor(Color.white, .5f).SetDelay(3.1f);
-        Beatrice.DOColor(Color.white, .5f).SetDelay(3.4f);
-        Benedick.DOColor(Color.white, .5f).SetDelay(3.7f);
-        Hamlet.DOColor(Color.white, .5f).SetDelay(4f);
+        Juliet.DOColor(Color.white, .5f).SetDelay(3.4f);
+        Hamlet.DOColor(Color.white, .5f).SetDelay(3.7f);
+        Romeo.DOColor(Color.white, .5f).SetDelay(4f);
         Ophelia.DOColor(Color.white, .5f).SetDelay(4.3f);
-        Juliet.DOColor(Color.white, .5f).SetDelay(4.6f);
-        Romeo.DOColor(Color.white, .5f).SetDelay(4.9f);
+        Benedick.DOColor(Color.white, .5f).SetDelay(4.6f);
+        Beatrice.DOColor(Color.white, .5f).SetDelay(4.9f);
         Petruchio.DOColor(Color.white, .5f).SetDelay(5.2f);
         Kate.DOColor(Color.white, .5f).SetDelay(5.5f);
         StartCoroutine(DelayedButton(5.5f));
